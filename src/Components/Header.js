9c80738b9867dbeedab023f67382d11539708a19
DanPisarenko <daniil.alex.p@gmail.com>
2023-02-22 09:46:56 +0300

    upd

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,10 @@
 import './style.css';
+import React, {useState} from "react";
+
 export default function Header({className,id, bgc, txt_color ,children, logo}){
+
+    const [open, setOpen] = useState(false)
+
     return(
         <header className={`hgjkxft ${className}`} style={{backgroundColor: bgc, color: txt_color}}>
             <div className="ffghjhfsgdf">
@@ -9,8 +14,22 @@ export default function Header({className,id, bgc, txt_color ,children, logo}){
             {children}
             </div>
             <div className="hhddgh_aa">
-            {children}
+            <div className="menu-trigger" onClick={()=>{setOpen(!open)}}>
+            <i className="rrr_font fa-sharp fa-solid fa-caret-down"></i>
+                        <div className={`dropdown-menu ${open? 'active': 'inactive'}`}>
+                                
+                   <DropdownItem ch={children}/>
+                
+            </div>
+            </div>
             </div>
         </header>
+    )
+}
+function DropdownItem(props) {
+    return(
+        <li className="dropdownItem">
+            {props.ch}
+        </li>
     )
 }
