8cad8cc3b0baab50a871d05b3025abd278844561
DanPisarenko <daniil.alex.p@gmail.com>
2023-03-17 13:37:32 +0300

    aaa

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 import Form from './Components/Form';
 function App() {
 
-
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert('You have submitted the form.')
+  }
 
   return (
     <div className="App">
@@ -12,7 +15,7 @@ function App() {
         <a href='https://andyuz27.github.io/aaa_blog/'>блог</a>
         <a href='https://aaa-account.vercel.app/users/635bcfb01aad8451105b085c'>Дезигн</a>*/}
       </Header>
-      <Form date1_inp_dsp="none" submit_txt="hello world"/>
+      <Form date1_inp_dsp="none" submit_txt="hello world" action={handleSubmit}/>
     </div>
   );
 }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -10,6 +10,8 @@ function Form({useenm_inp_dsp,
     email_inp_dsp,
     submit_dsp,
     submit_txt,
+    action,
+
     className, id,
     method}){
     return <div className='d_form'
@@ -20,26 +22,34 @@ function Form({useenm_inp_dsp,
         
     }}>
         <form method={method} className="gff" style={{
-            backgroundColor: "lime",
+            backgroundColor: "azure",
             paddingLeft:"15px",
             paddingRight:"15px",
             paddingBottom:"15px",
             display:"flex",
             flexDirection:"column",
             justifyContent:"center",
-            alignItems:"center"
-        }}>
+            alignItems:"center",
+            boxShadow: "15px 15px 15px #ddd",
+            borderRadius: "5%"
+        }} onSubmit={action}>
         <label for="usernm" style={{display: useenm_inp_dsp}}>Name:</label>
             <input type="text" style={{display: useenm_inp_dsp}} id="usernm"></input>
             <br></br>
             <label for="pwde" style={{display: pwd_inp_dsp}}>Password:</label>
             <input type="password" style={{display: pwd_inp_dsp}} id="pwde"></input>
             <br></br>
-            <label for="date" style={{display: date1_inp_dsp}}>date:</label>
+            <label for="date" style={{display: date1_inp_dsp}}>date 1:</label>
             <input type="date" style={{display: date1_inp_dsp}} id="date"></input>
+            <br></br>
+            <label for="date" style={{display: date2_inp_dsp}}>date 2:</label>
+            <input type="date" style={{display: date2_inp_dsp}} id="date"></input>
             <br></br>
             <label for="email" style={{display: email_inp_dsp}}>E-Mail:</label>
             <input type="email" style={{display: email_inp_dsp}} id="email"></input>
+            <br></br>
+            <label for="text" style={{display: text_inp_dsp}}>Type text here:</label>
+            <input type="text" style={{display: text_inp_dsp}} id="email"></input>
             <br></br>
             <input type="submit" style={{display: submit_dsp}} id="smt" value={submit_txt}></input>
         </form>
